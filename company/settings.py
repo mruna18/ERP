a6c60f39faf961135686dd7c055650f5547a3363
Mrunali <mrunaliparsekar18@gmail.com>
2025-07-05 18:29:43 +0530

    added

--- company/settings.py
+++ company/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','customer'
+    'django.contrib.staticfiles','customer','rest_framework',
+'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH_USER_MODEL = 'customer.User' #!
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import RegisterSerializer
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            user = User.objects.create_user(
+                username=data['username'],
+                email=data['email'],
+                password=data['password'],
+                first_name=data.get('first_name', ''),
+                last_name=data.get('last_name', '')
+            )
+            return Response({'message': 'User registered successfully', 'status':200})
+        return Response(serializer.errors, status=400)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        try:
+            # Authenticate 
+            user = User.objects.get(email=email)
+            user = authenticate(username=user.username, password=password)
+
+            if user:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                    'token': token.key,
+                    'username': user.username,
+                    'email': user.email
+                })
+            else:
+                return Response({'error': 'Invalid credentials'}, status=401)
+        except User.DoesNotExist:
+            return Response({'error': 'User with this email does not exist'}, status=404)
